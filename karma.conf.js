5c2c375aa90f4f9b44e51a0ff19947ec2db0f7dc
Johnny Silva <johnnysilva.dev@gmail.com>
2022-05-31 11:54:07 -0300

    coverage

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,8 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-junit-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -28,8 +30,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome',],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    CUSTOMLAUNCHERS: {
+      FIREFOXSEMCABECA: {
+        BASE: 'FIREFOX',
+        FLAGS: ['-HEADLESS']
+      }
+    }
   });
 };
