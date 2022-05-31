5e394dd0f3d924b3fa1b6cf1288d78d8de195377
Johnny Silva <johnnysilva.dev@gmail.com>
2022-05-31 10:41:56 -0300

    assercoes assincronas

--- src/app/shared/components/like-widget/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget/like-widget.component.spec.ts
@@ -1,14 +1,17 @@
+import { LikeWidgetModule } from './like-widget.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from 'src/app/shared/services/unique-id/unique-id.service';
 
 import { LikeWidgetComponent } from './like-widget.component';
 
-describe('LikeWidgetComponent', () => {
+describe(LikeWidgetComponent.name, () => {
   let component: LikeWidgetComponent;
   let fixture: ComponentFixture<LikeWidgetComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
+      imports: [LikeWidgetModule]
     })
     .compileComponents();
   });
@@ -16,10 +19,32 @@ describe('LikeWidgetComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should auto generate Id when id input property is missing', () => {
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy
+  });
+
+  it('should NOT genererated Id when id input property is present', () => {
+    const someId = "someId";
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBe(someId);
+  });
+
+  it(`${LikeWidgetComponent.prototype.like.name}
+    should trigger emission when called`, (done) => {
+      fixture.detectChanges();
+      component.liked.subscribe(() => {
+        expect(true).toBeTrue();
+        done();
+      });
+      component.like();
+    });
 });
--- src/app/shared/components/like-widget/like-widget/like-widget.module.ts
+++ src/app/shared/components/like-widget/like-widget/like-widget.module.ts
@@ -1,3 +1,4 @@
+import { UniqueIdService } from './../../../services/unique-id/unique-id.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LikeWidgetComponent } from './like-widget.component';
@@ -13,6 +14,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     CommonModule,
     FontAwesomeModule
   ],
-  exports: [LikeWidgetComponent]
+  exports: [LikeWidgetComponent],
+  providers: [ UniqueIdService]
 })
 export class LikeWidgetModule { }
